8b98a7425720416e9d9d94f40221b74c06181599
2282461712 <2282461712@qq.com>
2019-05-29 21:59:09 +0800

    添加c3、c4

---
+++ module/src/main/java/com/szxs/dto/VoSlideShowPic.java
@@ -0,0 +1,40 @@
+package com.szxs.dto;
+
+public class VoSlideShowPic {
+    private Integer id;
+    private String itemName;
+    private double minPrice;
+    private String imgUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(double minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
